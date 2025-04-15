d26b37bd3edc2ccd77cda41e46f60218047721a6
fido1hn <confidoekeanya@gmail.com>
2025-04-15 22:33:28 +0100

    pancake movement and collision

--- src/lib.rs
+++ src/lib.rs
@@ -25,13 +25,34 @@ turbo::go!({
         state.pancakes.push(pancake);
     }
 
+    // Move falling pancakes and detect when they're caught
+    let cat_center = (state.cat_x + state.cat_r, state.cat_y + state.cat_r);
+    state.pancakes.retain_mut(|p| {
+        p.y += p.vel;
+        let dx = cat_center.0 - (p.x + p.radius);
+        let dy = cat_center.1 - (p.y + p.radius);
+        let distance = (dx * dx + dy * dy).sqrt();
+        let radii_sum = state.cat_r + p.radius;
+        let radii_diff = (state.cat_r - p.radius).abs();
+
+        if radii_diff <= distance && distance <= radii_sum {
+            state.score += 1;
+            state.last_munch_at = state.frame;
+            false
+        } else if p.y < 144. + (p.radius * 2.) {
+            true
+        } else {
+            false
+        }
+    });
+
     state.save();
 });
 
 turbo::init! {
     struct GameState {
         frame: u32,
-        last_much_at: u32,
+        last_munch_at: u32,
         cat_x: f32,
         cat_y: f32,
         cat_r: f32,
@@ -44,7 +65,7 @@ turbo::init! {
         score: u32,
     } = Self {
         frame: 0,
-        last_much_at: 0,
+        last_munch_at: 0,
         cat_x: 128.0,
         cat_y: 112.0,
         cat_r:8.0,
