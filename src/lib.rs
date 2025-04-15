84ab6fea6f1f46f21e1daa58ab848719a97f8534
fido1hn <confidoekeanya@gmail.com>
2025-04-15 21:32:18 +0100

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+// This is where your main game loop code goes
+// The stuff in this block will run ~60x per sec
+turbo::go!({
+    text!(
+        "Only possible on solana",
+        font = "medium",
+        color = 0xff00ffff
+    );
+});
