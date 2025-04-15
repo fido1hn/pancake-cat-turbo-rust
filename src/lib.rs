7f5d6f868974813fed42e2d88ba835b20b928206
fido1hn <confidoekeanya@gmail.com>
2025-04-15 21:48:12 +0100

    added sprite and game play for player

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,44 @@
 // This is where your main game loop code goes
 // The stuff in this block will run ~60x per sec
 turbo::go!({
-    text!(
-        "Only possible on solana",
-        font = "medium",
-        color = 0xff00ffff
-    );
+    let mut state = GameState::load();
+    text!("Game state loaded");
+
+    // Game play for player
+    if gamepad(0).left.pressed() {
+        state.cat_x -= 2.
+    }
+
+    if gamepad(0).right.pressed() {
+        state.cat_x += 2.
+    }
+
+    // randomly spawned pancake
+
+    state.save();
 });
+
+turbo::init! {
+    struct GameState {
+        frame: u32,
+        last_much_at: u32,
+        cat_x: f32,
+        cat_y: f32,
+        cat_r: f32,
+        pancakes: Vec<struct Pankcake {
+            x: f32,
+            y: f32,
+            vel: f32,
+            radius:f32,
+        }>,
+        score: u32,
+    } = Self {
+        frame: 0,
+        last_much_at: 0,
+        cat_x: 128.0,
+        cat_y: 112.0,
+        cat_r:8.0,
+        pancakes: vec![],
+        score: 0
+    }
+}
